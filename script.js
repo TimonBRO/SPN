d39d45659c35628b4ab1ac9e5d03ff6c3fc69a19
unknown <timon.connect@gmail.com>
2019-12-08 15:35:06 +0300

    21

---
+++ script.js
@@ -0,0 +1,29 @@
+var a = document.getElementById("Item1"),
+	b = document.getElementById("Item2"),
+	c = document.getElementById("Item3"),
+	d = document.getElementById("Item4"),
+	e = document.getElementById("Item5"),
+	f = document.getElementById("Item6"),
+	g = document.getElementById("Item7"),
+	h = document.getElementById("txt1"),
+	k = document.getElementById("txt2"),
+	l = document.getElementById("txt3"),
+	m = document.getElementById("txt4"),
+	n = document.getElementById("txt5"),
+	p = document.getElementById("txt6"),
+	q = document.getElementById("txt7");
+a.addEventListener("click", function() {
+	h.style.visibility = "visible", k.style.visibility = "hidden", l.style.visibility = "hidden", m.style.visibility = "hidden", n.style.visibility = "hidden", p.style.visibility = "hidden", q.style.visibility = "hidden", a.style.background = "rgb(229, 180, 250)", b.style.background = "aquamarine", c.style.background = "aquamarine", d.style.background = "aquamarine", e.style.background = "aquamarine", f.style.background = "aquamarine", g.style.background = "aquamarine"
+}), b.addEventListener("click", function() {
+	a.style.background = "aquamarine", b.style.background = "rgb(229, 180, 250)", c.style.background = "aquamarine", d.style.background = "aquamarine", e.style.background = "aquamarine", f.style.background = "aquamarine", g.style.background = "aquamarine", h.style.visibility = "hidden", k.style.visibility = "visible", l.style.visibility = "hidden", m.style.visibility = "hidden", n.style.visibility = "hidden", p.style.visibility = "hidden", q.style.visibility = "hidden"
+}), c.addEventListener("click", function() {
+	a.style.background = "aquamarine", b.style.background = "aquamarine", c.style.background = "rgb(229, 180, 250)", d.style.background = "aquamarine", e.style.background = "aquamarine", f.style.background = "aquamarine", g.style.background = "aquamarine", h.style.visibility = "hidden", k.style.visibility = "hidden", l.style.visibility = "visible", m.style.visibility = "hidden", n.style.visibility = "hidden", p.style.visibility = "hidden", q.style.visibility = "hidden"
+}), d.addEventListener("click", function() {
+	a.style.background = "aquamarine", b.style.background = "aquamarine", c.style.background = "aquamarine", d.style.background = "rgb(229, 180, 250)", e.style.background = "aquamarine", f.style.background = "aquamarine", g.style.background = "aquamarine", h.style.visibility = "hidden", k.style.visibility = "hidden", l.style.visibility = "hidden", m.style.visibility = "visible", n.style.visibility = "hidden", p.style.visibility = "hidden", q.style.visibility = "hidden"
+}), e.addEventListener("click", function() {
+	a.style.background = "aquamarine", b.style.background = "aquamarine", c.style.background = "aquamarine", d.style.background = "aquamarine", e.style.background = "rgb(229, 180, 250)", f.style.background = "aquamarine", g.style.background = "aquamarine", h.style.visibility = "hidden", k.style.visibility = "hidden", l.style.visibility = "hidden", m.style.visibility = "hidden", n.style.visibility = "visible", p.style.visibility = "hidden", q.style.visibility = "hidden"
+}), f.addEventListener("click", function() {
+	a.style.background = "aquamarine", b.style.background = "aquamarine", c.style.background = "aquamarine", f.style.background = "aquamarine", e.style.background = "aquamarine", f.style.background = "rgb(229, 180, 250)", g.style.background = "aquamarine", h.style.visibility = "hidden", k.style.visibility = "hidden", l.style.visibility = "hidden", m.style.visibility = "hidden", n.style.visibility = "hidden", p.style.visibility = "visible", q.style.visibility = "hidden"
+}), g.addEventListener("click", function() {
+	a.style.background = "aquamarine", b.style.background = "aquamarine", c.style.background = "aquamarine", d.style.background = "aquamarine", e.style.background = "aquamarine", f.style.background = "aquamarine", g.style.background = "rgb(229, 180, 250)", h.style.visibility = "hidden", k.style.visibility = "hidden", l.style.visibility = "hidden", m.style.visibility = "hidden", n.style.visibility = "hidden", p.style.visibility = "hidden", q.style.visibility = "visible"
+});
